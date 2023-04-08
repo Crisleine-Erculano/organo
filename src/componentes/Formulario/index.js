603b87cf13560585df8980b8fed8eb90a209c3c9
Crisleine Erculano <candidocrislaine576@gmail.com>
2023-04-08 18:29:41 -0300

    Criando botão do formulário.

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,6 +1,7 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
 
 const Formulario = () => {
    const times = [
@@ -16,10 +17,16 @@ const Formulario = () => {
       <section className="formulario">
          <form>
             <h2>Preencha os campos para criar os cards do colaborador</h2>
+            
             <CampoTexto label="Nome" placeholder="Digite o seu nome." />
             <CampoTexto label="Cargo" placeholder="Digite o seu Cargo." />
             <CampoTexto label="Imagem" placeholder="Digite o endereço da  imagem." />
-            <ListaSuspensa label="Time" itens={times} />
+            
+            <ListaSuspensa label="Time" itens={times}/>
+            
+            <Botao>
+               Criar Card
+            </Botao>
          </form>
       </section>
    )
