1091e9fdd1ba2534c55681b16a2c339be85e24e6
Crisleine Erculano <candidocrislaine576@gmail.com>
2023-04-15 11:02:29 -0300

    Alterando cor de fundo do card.

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -4,18 +4,20 @@ import './Time.css'
 const Time = (props) => {
    const colorCss = { backgroundColor: props.corSecundaria }
    return (
-     
+
       <section className='time' style={colorCss}>
          <h3 style={{ borderColor: props.corPrimaria }}>{props.nome} </h3>
          <div className='colaboradores'>
-         {props.colaboradores.map(colaborador => <Colaborador key={colaborador.nome}
-            nome={colaborador.nome}
-            cargo={colaborador.cargo}
-            imagem={colaborador.imagem}
-             />)}
+            {props.colaboradores.map(colaborador => <Colaborador
+               key={colaborador.nome}
+               corDeFundo={props.corPrimaria}
+               nome={colaborador.nome}
+               cargo={colaborador.cargo}
+               imagem={colaborador.imagem}
+            />)}
          </div>
       </section>
-      
+
 
    )
 }
