79944180690fd378a55b057824d8877c29f3ff70
Crisleine Erculano <candidocrislaine576@gmail.com>
2023-04-17 10:34:22 -0300

    Corrigindo caminho da imagem do Banner.

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -2,7 +2,7 @@ import './Banner.css'
 function Banner(){
    return (
       <header className="banner">
-         <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+         <img src="./imagens/banner.png" alt="O banner principal da página do Organo"/>
       </header>
   )
 }
